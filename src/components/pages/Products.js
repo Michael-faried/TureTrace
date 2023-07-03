210065ed7875179e9373a049a686ee288ae0dbb1
Philip Wagih <philipwagih9@gmail.com>
2023-07-03 22:13:25 +0300

    products is complete, display by model

--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -10,14 +10,13 @@ import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme, responsiveFontSizes, ThemeProvider } from '@mui/material/styles';
 import { useLocation } from 'react-router';
 import { useContext } from 'react';
 import { CompanyContext } from '../CompanyContext';
 import { retrieve_company_products } from '../../web3Client';
 import image from '../../backgrounds/back1.png';
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const defaultTheme = createTheme();
 
 export default function Products() {
@@ -35,6 +34,17 @@ export default function Products() {
       });
   }, [companyName2]);
 
+  // Group products by model and count the occurrences
+  const groupedProducts = products.reduce((accumulator, product) => {
+    const existingProduct = accumulator.find((p) => p.model === product.model);
+    if (existingProduct) {
+      existingProduct.count++;
+    } else {
+      accumulator.push({ ...product, count: 1 });
+    }
+    return accumulator;
+  }, []);
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <CssBaseline />
@@ -47,24 +57,19 @@ export default function Products() {
       >
         <Box sx={{ pt: 6 }}>
           <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-            >
+            <Typography component="h1" variant="h2" align="center" color="text.primary">
               {companyName2}
             </Typography>
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
-              Company Info
+              Total Number of Products: {products.length}
             </Typography>
             <Stack sx={{ pt: 4 }} direction="row" spacing={2} justifyContent="center" />
           </Container>
         </Box>
 
-        <Container sx={{ py: 4 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {products.map((product) => (
+        <Container sx={{ py: 4, maxWidth: 'md' }}>
+          <Grid container spacing={4} justifyContent="center">
+            {groupedProducts.map((product) => (
               <Grid item key={product.id} xs={12} sm={6} md={4}>
                 <Card
                   sx={{
@@ -78,19 +83,25 @@ export default function Products() {
                     component="div"
                     sx={{
                       // 16:9
-                      pt: '56.25%',
+                      pt: '52.25%',
                     }}
                     image={product.imageLink}
                   />
-                  <CardContent sx={{ flexGrow: 1 }}>
+                  <CardContent sx={{ flexGrow: 1, pb: 0 }}>
                     <Typography gutterBottom variant="h5" component="h2">
                       {product.name}
                     </Typography>
-                    <Typography>{product.description}</Typography>
+                    <Typography variant="body2" sx={{ pt: 2, pb: 0 }}>
+                      {product.description}
+                    </Typography>
                   </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
+                  <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                      {product.count > 1 ? `Count: ${product.count}` : ''}
+                    </Typography>
+                    <Button size="small" sx={{ color: 'red' }}>
+                      Delete
+                    </Button>
                   </CardActions>
                 </Card>
               </Grid>
